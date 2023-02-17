b0be32c8bad98e65d27de99b660e0ac61a443868
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-02-17 11:54:32 +0200

    add edits

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,51 +1,20 @@
-import { Component, useEffect } from 'react';
+import { useEffect } from 'react';
 import css from './Modal.module.css';
-
-// export class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   handleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//       this.props.closeModal();
-//     }
-//   };
-//   handleBackdrop = e => {
-//     if (e.target === e.currentTarget) {
-//       this.props.closeModal();
-//     }
-//   };
-
-//   render() {
-//     return (
-//       <div className={css.Overlay} onClick={this.handleBackdrop}>
-//         <div className={css.Modal}>
-//           <img src={this.props.largeImageURL} alt="LargeImages" />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 export function Modal({ largeImageURL, closeModal }) {
   useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    };
+
     window.addEventListener('keydown', handleKeyDown);
-  }, [handleKeyDown]);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [closeModal]);
 
-  useEffect(() => {
-    window.removeEventListener('keydown', handleKeyDown);
-  }, [handleKeyDown]);
-
-  function handleKeyDown(e) {
-    if (e.code === 'Escape') {
-      closeModal();
-    }
-  }
   const handleBackdrop = e => {
     if (e.target === e.currentTarget) {
       closeModal();
